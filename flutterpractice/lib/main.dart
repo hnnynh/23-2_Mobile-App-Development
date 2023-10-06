334a7c026ce4c133e18596842614e1e4504d6f5e
hnnynh <hnnynh125@gmail.com>
2023-10-06 14:24:52 +0900

    week4: assginment

--- flutterpractice/lib/main.dart
+++ flutterpractice/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'style.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First Flutter App',
       theme: ThemeData(
-        primaryColor:  Colors.blue,
-        // primarySwatch: Colors.orange,
+        primaryColor: Colors.blue,
+        primarySwatch: Colors.indigo,
         fontFamily: 'Pretendard',
       ),
       home: MyHomePage(),
@@ -25,69 +25,159 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
-  // final items = List.generate(100, (index) => index).toList();   // 0 ~ 99까지 넣기
-  
+  final dogs = List.generate(16, (index) => index+1).toList();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.Malachite,
-        title: const Text('Assignment 2'),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.person))
-        ],
-      ),
-      body: Center(
-        child: Column(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Assignment 3'),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.image)),
+              Tab(icon: Icon(Icons.how_to_vote)),
+              Tab(icon: Icon(Icons.upload)),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            const Stack(
-              alignment: Alignment.center,
-              children: [
-                Opacity(
-                  opacity: 0.2,
-                  child: Image(
-                    image: AssetImage(
-                        'assets/warning.jpeg'
-                    ),
-                    width: 300.0,
-                  ),
-                ),
-
-                Text('This page does not include contents'),
-              ],
+            Tab(
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: dogs.map((i) =>
+                    Image.asset('dog_$i.jpeg'),
+                ).toList(),
+              ),
             ),
-            Expanded(
-                child:
-                ListView(
-                  scrollDirection: Axis.vertical,
-                  children: [
-                    ListTile(
-                      leading: const Icon(Icons.home),
-                      title: const Text('Home'),
-                      trailing: const Icon(Icons.navigate_next),
-                      onTap: (){},
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.calendar_month),
-                      title: const Text('Calendar'),
-                      trailing: const Icon(Icons.navigate_next),
-                      onTap: (){},
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.camera),
-                      title: const Text('Camera'),
-                      trailing: const Icon(Icons.navigate_next),
-                      onTap: (){},
-                    ),
-                  ],
-                ),
+            Tab(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Text("Candidate"),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 4.0,
+                          child: Container(
+                              height: 100,
+                              child: const Center(
+                                child: Text(
+                                  "Dog1",
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              )),
+                        ),
+                      ),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 4.0,
+                          child: Container(
+                              height: 100,
+                              child: const Center(
+                                child: Text(
+                                  "Dog2",
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                              )),
+                        ),
+                      ),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 4.0,
+                          child: Container(
+                              height: 100,
+                              child: const Center(
+                                child: Text(
+                                  "Dog2",
+                                  style: TextStyle(color: Colors.green),
+                                ),
+                              )),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Text("Vote Rate"),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: Container(
+                          height: 100,
+                          color: Colors.red,
+                          child: const Center(
+                            child: Text(
+                              "50%",
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          height: 100,
+                          color: Colors.blue,
+                          child: const Center(
+                            child: Text(
+                              "20%",
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          color: Colors.green,
+                          height: 100,
+                          child: const Center(
+                            child: Text(
+                              "30%",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Tab(
+              child: LinearProgressIndicator(),
             ),
           ],
         ),
+        drawer: Drawer(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.photo_album), label: 'album'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'settings'),
+          ],
+        ),
       ),
-      drawer: Drawer(),
     );
   }
 }
