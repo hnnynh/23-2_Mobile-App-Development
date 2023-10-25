657cd009d7fe72837c03bb07d5490749d7d38d01
hnnynh <hnnynh125@gmail.com>
2023-10-25 14:46:51 +0900

    week6: assignment
    StatefulWidget, GestureDetector

--- flutterpractice/lib/main.dart
+++ flutterpractice/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+List<StudentInfo> studentList = [];
+
 void main() {
   runApp(MyApp());
 }
@@ -12,8 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Grade Calculator',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
-
+        primarySwatch: Colors.purple,
       ),
       home: MyHomePage(),
     );
@@ -23,184 +24,226 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
-
-  final _midController = TextEditingController();
-  final _finalController = TextEditingController();
-  final _pointList = List.generate(10, (i) => i);
-  var _selectedValue = 0;
-  bool _isLeader = false;
-  bool _isAbsenceLess4 = true;
-  String _grade = 'B';
-
-  void dispose(){
-    _midController.dispose();
-    _finalController.dispose();
-    super.dispose();
-  }
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
-  Widget build(BuildContext context){
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Receive User Information'),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-            child: ListView(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Midterm Exam'
-                  ),
-                  controller: _midController,
-                  keyboardType: TextInputType.number,
-                ),
-                Container(
-                  height: 20,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Final Exam'
-                  ),
-                  controller: _finalController,
-                  keyboardType: TextInputType.number,
-                ),
-                Container(
-                  height: 20,
-                ),
-                RadioListTile(
-                    title: Text('Project Team Leader (+10)'),
-                    value: true,
-                    groupValue: _isLeader,
-                    onChanged: (value){
-                      setState(() {
-                        _isLeader = value!;
-                      });
-                    }
-                ),
-                RadioListTile(
-                    title: Text('Project Team Member'),
-                    value: false,
-                    groupValue: _isLeader,
-                    onChanged: (value){
-                      setState(() {
-                        _isLeader = value!;
-                      });
-                    }
-                ),
-                Container(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    const Expanded(
-                      flex: 7,
-                      child: Text(
-                          'Additional Point',
-                          textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: DropdownButton(
-                          isExpanded: true,
-                          alignment: Alignment.centerRight,
-                          value: _selectedValue,
-                          items: _pointList.map(
-                              (point) => DropdownMenuItem(
-                                  value: point,
-                                  child: Text('$point point'))).toList(),
-                        onChanged: (value) {
-                            setState(() {
-                              _selectedValue = value!;
-                            });
-                        },
-
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    const Expanded(
-                      flex: 9,
-                      child: Text(
-                          'Absence less than 4',
-                          textAlign: TextAlign.left,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Checkbox(
-                          value: _isAbsenceLess4,
-                          onChanged: (value){
-                            setState(() {
-                              _isAbsenceLess4 = value!;
-                            });
-                          }
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  height: 20,
-                ),
-                Text(
-                  _grade,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 25, color: Colors.orange),
-                ),
-                Container(
-                  height: 20,
-                ),
-                ElevatedButton(onPressed: (){
-                  setState(() {
-                    var midValue = double.parse(_midController.text.trim());
-                    var finalValue = double.parse(_finalController.text.trim());
-                    var sum = midValue + finalValue + _selectedValue;
-                    if(_isLeader) {
-                      sum += 10;
-                    }
-                    if(!_isAbsenceLess4){
-                      _grade = 'F';
-                    }
-                    else if(sum >= 170){
-                      _grade = 'A';
-                    }
-                    else if(sum >= 150){
-                      _grade = 'B';
-                    }
-                    else if(sum >= 130){
-                      _grade = 'C';
-                    }
-                    else if(sum >= 110){
-                      _grade = 'D';
-                    }
-                    else{
-                      _grade = 'F';
-                    }
-                  });
-                }, child: Text('Enter')),
-              ],
-            ),
-          
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Grade Calculator'),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Information'),
+              Tab(text: 'List'),
+            ],
+          ),
         ),
-        drawer: Drawer(),
+        body: TabBarView(
+          children: [
+            Tab(
+              child: MyForm(),
+            ),
+            Tab(
+                child: MyListPage(),
+            ),
+          ],
+        )
+        // drawer: Drawer(),
       ),
     );
   }
+}
 
+class MyForm extends StatefulWidget{
+  const MyForm({super.key});
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm>{
+
+  StudentInfo studentInfo = StudentInfo('', 0, 0);
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Name'
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                } else if (int.tryParse(value) != null) {
+                  return 'Please enter some string, not a number';
+                }
+                return null;
+              },
+              onSaved: (value){
+                studentInfo.name = value!;
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Project point'
+              ),
+              keyboardType: TextInputType.number,
+              validator: (value){
+                if (value == null || value.isEmpty) {
+                  return 'Please insert some text';
+                } else if (int.tryParse(value) == null) {
+                  return 'Please insert some integer';
+                }
+                return null;
+              },
+              onSaved: (value){
+                studentInfo.projectPoint = int.parse(value!);
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            DropdownButtonFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Additional Point',
+              ),
+              value: studentInfo.additionalPoint,
+              items: List.generate(11, (i) {
+                if (i == 0) {
+                  return DropdownMenuItem(
+                    value: i-1,
+                    child: Text('Choose the additional Point'),
+                  );
+                }
+                return DropdownMenuItem(
+                  value: i-1,
+                  child: Text('${i - 1} point'),
+                );
+              }),
+              onChanged: (value) {
+                setState(() {
+                  studentInfo.additionalPoint = value!;
+                });
+              },
+              validator: (value) {
+                if (value == -1) {
+                  return 'Please select the point';
+                }
+                return null;
+              },
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              child: Container(
+                  height: 60, color: Colors.indigo,
+                  child: const Center(
+                    child: Text('Enter',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  )),
+              onTap: () {
+                setState(() {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Processing data'),
+                    ));
+                    _formKey.currentState!.save();
+                    studentList.add(studentInfo);
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyListPage extends StatefulWidget {
+  const MyListPage({Key? key}) : super(key:key);
+
+  @override
+  State<MyListPage> createState() => _MyListPageState();
+}
+
+class _MyListPageState extends State<MyListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ReorderableListView.builder(
+      itemCount: studentList.length,
+      itemBuilder: (c, i){
+        return Dismissible(
+          key: ValueKey(studentList[i]),
+          background: Container(
+            color: Colors.green,
+          ),
+          child: ListTile(
+            leading: const Icon(Icons.home),
+            title: Text(studentList[i].toString()),
+          ),
+          onDismissed: (direction) {
+            setState(() {
+              studentList.removeAt(i);
+            });
+          },
+        );
+      },
+      onReorder: (oldIndex, newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          studentList.insert(newIndex, studentList.removeAt(oldIndex));
+        });
+      },
+    );
+  }
+}
+
+class StudentInfo {
+  String name;
+  int projectPoint;
+  int additionalPoint;
+
+  StudentInfo(
+      this.name,
+      this.projectPoint,
+      this.additionalPoint,
+      );
+
+  @override
+  String toString() {
+    return
+        '$name :'
+        '${projectPoint + additionalPoint}';
+  }
 }
