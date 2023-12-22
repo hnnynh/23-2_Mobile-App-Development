0a86aac9ced4504a58bf507bfbb8352fb5da3c1d
hnnynh <hnnynh125@gmail.com>
2023-12-22 12:02:51 +0900

    week9: assignment

--- flutterpractice/lib/LogInPage.dart
+++ flutterpractice/lib/LogInPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './RegisterPage.dart';
 import './ChatPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LogInPage extends StatelessWidget {
   const LogInPage({super.key});
@@ -25,6 +26,7 @@ class LogInForm extends StatefulWidget {
 }
 
 class _LogInFormState extends State<LogInForm> {
+  bool saving = false;
   final _authentication = FirebaseAuth.instance;
   final _formKey = GlobalKey<FormState>();
   String email = '';
@@ -32,58 +34,67 @@ class _LogInFormState extends State<LogInForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Email'
+    return ModalProgressHUD(
+      inAsyncCall: saving,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email'
+                ),
+                onChanged: (value){
+                  email = value;
+                },
               ),
-              onChanged: (value){
-                email = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                  labelText: 'Password'
+              const SizedBox(
+                height: 20,
               ),
-              onChanged: (value){
-                password = value;
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(onPressed: () async {
-              try{
-                final currentUser = await _authentication.signInWithEmailAndPassword(email: email, password: password);
-                if(currentUser.user != null){
-                  _formKey.currentState!.reset();
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: 'Password'
+                ),
+                onChanged: (value){
+                  password = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(onPressed: () async {
+                try{
+                  setState(() {
+                    saving = true;
+                  });
+                  final currentUser = await _authentication.signInWithEmailAndPassword(email: email, password: password);
+                  if(currentUser.user != null){
+                    _formKey.currentState!.reset();
+                  }
+                  setState(() {
+                    saving = false;
+                  });
+                }catch(e) {
+                  print(e);
                 }
-              }catch(e) {
-                print(e);
-              }
-            }, child: Text('Enter')),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text('If you did not register,'),
-                TextButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()));
-                }, child: Text('Register your email')),
-              ],
-            )
-          ],
+              }, child: Text('Enter')),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('If you did not register,'),
+                  TextButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()));
+                  }, child: Text('Register your email')),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
